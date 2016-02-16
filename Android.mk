4d76892b28a8085fb7466fd18c6d1836b3359d53
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2016-02-15 21:25:16 -0500

    cmsdk: Update vendor path
    Change-Id: I555d9c1f23a780f68314a8e5d62e5710cbb33a58
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- Android.mk
+++ Android.mk
@@ -287,7 +287,7 @@ LOCAL_SRC_FILES := $(cmplat_docs_src_files)
 LOCAL_ADDITONAL_JAVA_DIR := $(intermediates.COMMON)/src
 
 LOCAL_IS_HOST_MODULE := false
-LOCAL_DROIDDOC_CUSTOM_TEMPLATE_DIR := vendor/cm/build/tools/droiddoc/templates-cmsdk
+LOCAL_DROIDDOC_CUSTOM_TEMPLATE_DIR := vendor/pac/build/tools/droiddoc/templates-cmsdk
 LOCAL_ADDITIONAL_DEPENDENCIES := \
     services \
     org.cyanogenmod.hardware
